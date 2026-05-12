2cd5a6ca680e94641619c9cc9947daf8c13ef88b
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-05-12 20:48:36 +0530

    Implement Word Break solution in Word_Break.py

---
+++ Leetcode_Practice/Array/Word_Break.py
@@ -0,0 +1,24 @@
+# 139. Word Break
+
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+
+        wordSet = set(wordDict)
+        n = len(s)
+
+        # dp[i] means string till index i can be segmented
+        dp = [False] * (n + 1)
+        dp[0] = True
+
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordSet:
+                    dp[i] = True
+                    break
+
+        return dp[n]
