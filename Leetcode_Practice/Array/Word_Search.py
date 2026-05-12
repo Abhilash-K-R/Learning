4eef25978358cd6d2b31425d97dd82094cf4df9d
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-05-12 19:30:21 +0530

    Add Word Search solution using DFS
    Implement depth-first search to find the word in the board.

---
+++ Leetcode_Practice/Array/Word_Search.py
@@ -0,0 +1,47 @@
+# 79. Word Search
+
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+
+        rows = len(board)
+        cols = len(board[0])
+
+        def dfs(r, c, index):
+            # word found
+            if index == len(word):
+                return True
+
+            # out of bounds or character mismatch
+            if (r < 0 or c < 0 or
+                r >= rows or c >= cols or
+                board[r][c] != word[index]):
+                return False
+
+            # mark visited
+            temp = board[r][c]
+            board[r][c] = "#"
+
+            # explore 4 directions
+            found = (
+                dfs(r + 1, c, index + 1) or
+                dfs(r - 1, c, index + 1) or
+                dfs(r, c + 1, index + 1) or
+                dfs(r, c - 1, index + 1)
+            )
+
+            # backtrack
+            board[r][c] = temp
+
+            return found
+
+        for i in range(rows):
+            for j in range(cols):
+                if dfs(i, j, 0):
+                    return True
+
+        return False
